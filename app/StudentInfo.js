6169aaf7156cafd00a217e63055609146d8b1bd9
Zainul-Malik <zainulmalik2004@gmail.com>
2023-09-19 13:24:14 -0600

    Create StudentInfo.js

---
+++ app/StudentInfo.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+export default function StudentInfo() 
+{
+    return (
+        <main>
+            <div>
+                <p>Zainul Malik</p>
+                <p>CPRG 306 A</p>
+            </div>
+        </main>
+    )
+}
+
+
+
+
