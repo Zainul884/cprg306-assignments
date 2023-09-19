b8720ecf2a6f759d7409ec7e8ed984b3fa70a2e9
Zainul-Malik <zainulmalik2004@gmail.com>
2023-09-19 13:59:15 -0600

    Finishing Steps

--- app/week2/page.js
+++ app/week2/page.js
@@ -2,4 +2,13 @@ import Link from "next/link"
 import StudentInfo from "../StudentInfo"
 
 
-<h1>My Shopping List</h1>
+export default function Week2() {
+    return (
+        <main>
+            <div>
+                <h1>My Shopping List</h1>
+                <StudentInfo />
+            </div>
+        </main>
+    )
+}
