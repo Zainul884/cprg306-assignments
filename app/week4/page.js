2f0fccaf8c8286da23a21c723c727d509b2545ce
Zainul-Malik <zainulmalik2004@gmail.com>
2023-10-08 11:15:17 -0600

    week4_assignment

---
+++ app/week4/page.js
@@ -0,0 +1,13 @@
+// app/week4/page.js
+import React from 'react';
+import NewItem from './new-item';
+
+function Week4Page() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center">
+      <NewItem />
+    </div>
+  );
+}
+
+export default Week4Page;
