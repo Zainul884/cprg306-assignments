3fa43e8baf14cb5d36fafa1eaf7f9bdfee915005
Zainul Malik <zainul.malik@edu.sait.ca>
2023-10-12 23:44:34 -0600

    ASDASD
    ASDASDASDASD

---
+++ app/123456/123.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+import StudentInfo from "../StudentInfo"
+
+
+export default function Week2() {
+    return (
+        <main>
+            <div>
+                <h1>My Shopping List</h1>
+                <StudentInfo />
+            </div>
+        </main>
+    )
+}
