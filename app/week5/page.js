960db5f4edb8f508c745e20157179711ef2a9923
Zainul Malik <zainul.malik@edu.sait.ca>
2023-10-15 21:16:04 -0600

    week 5 assignment

---
+++ app/week5/page.js
@@ -0,0 +1,14 @@
+use client;
+import React from 'react';
+import ItemList from './item-list';
+
+function Page() {
+    return (
+        <main className="bg-gray-100 min-h-screen flex flex-col items-center justify-center py-8">
+            <h1 className="text-4xl font-bold text-gray-800 mb-6">Shopping List</h1>
+            <ItemList />
+        </main>
+    );
+}
+
+export default Page;
