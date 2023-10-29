f42994097bb1f3806a05a84537b95f8a7becd41c
Zainul884 <zainul.malik@edu.sait.ca>
2023-10-28 21:32:23 -0600

    week 2 fixed

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,12 +1,13 @@
-import Link from "next/link"
-import StudentInfo from "../StudentInfo"
-
+import Link from "next/link";
+import StudentInfo from "../StudentInfo";
 
 export default function Week2() {
     return (
-        <main>
-            <div>
-                <h1>My Shopping List</h1>
+        <main className="bg-gray-100 min-h-screen flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg shadow-md max-w-md mx-auto text-center">
+                <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+                    My Shopping List
+                </h1>
                 <StudentInfo />
             </div>
         </main>
