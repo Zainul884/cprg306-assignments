24b525244f81bc4a658aa87fc5ed2b7b833ce17c
Zainul884 <zainul.malik@edu.sait.ca>
2023-10-28 23:39:13 -0600

    fixed week 7 and week 8

--- app/week8/shopping-list/page.js
+++ app/week8/shopping-list/page.js
@@ -34,22 +34,20 @@ function Page() {
     };
 
     return (
-        <main className="bg-gray-100 min-h-screen flex items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-md max-w-3xl mx-auto text-center">
-                <h1 className="text-3xl font-bold text-gray-800 mb-4">Shopping List</h1>
-                <div style={{ display: 'flex', justifyContent: 'center', width: '100%', gap: '20px' }}>
-                    <div>
-                        <h2 className="text-2xl font-bold text-gray-800 mb-4">Add New Item</h2>
-                        <NewItem onAddItem={handleAddItem} />
-                        <ItemList items={items} onItemSelect={handleItemSelect} />
-                    </div>
-                    <div>
-                        <h2 className="text-2xl font-bold text-gray-800 mb-4">Meal Ideas</h2>
-                        <MealIdeas ingredient={selectedItemName} />
-                    </div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">Shopping List</h1>
+            <div style={{ display: 'flex', justifyContent: 'center', width: '100%', gap: '20px' }}>
+                <div>
+                    <h2 className="text-2xl font-bold text-gray-800 mb-4">Add New Item</h2>
+                    <NewItem onAddItem={handleAddItem} />
+                    <ItemList items={items} onItemSelect={handleItemSelect} />
+                </div>
+                <div>
+                    <h2 className="text-2xl font-bold text-gray-800 mb-4">Meal Ideas</h2>
+                    <MealIdeas ingredient={selectedItemName} />
                 </div>
             </div>
-        </main>
+        </div>
     );
 }
 
