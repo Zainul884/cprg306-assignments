bcfc10ad050ac1eec353dfbbd6c14927e95fd8f5
Zainul884 <zainul.malik@edu.sait.ca>
2023-11-20 13:12:30 -0700

    week 10 add items fixed

--- app/week10/_services/shopping-list-service.js
+++ app/week10/_services/shopping-list-service.js
@@ -1,21 +1,21 @@
 import { db } from "../_utils/firebase";
-import { collection, getDocs, addDoc, query } from "firebase/firestore";
+import { collection, getDocs, addDoc, query, deleteDoc, doc } from "firebase/firestore";
 
-async function getItems(userId) {
+export async function getItems(userId) {
     const items = [];
-    const itemsCollectionRef = collection(db, "users", userId, "items");
-    const q = query(itemsCollectionRef);
+    const q = query(collection(db, "users", userId, "items"));
     const querySnapshot = await getDocs(q);
-
     querySnapshot.forEach((doc) => {
         items.push({ id: doc.id, ...doc.data() });
     });
-
     return items;
 }
 
-async function addItem(userId, item) {
-    const itemsCollectionRef = collection(db, "users", userId, "items");
-    const docRef = await addDoc(itemsCollectionRef, item);
+export async function addItem(userId, item) {
+    const docRef = await addDoc(collection(db, "users", userId, "items"), item);
     return docRef.id;
+}
+
+export async function deleteItem(userId, itemId) {
+    await deleteDoc(doc(db, "users", userId, "items", itemId));
 }
--- app/week10/page.js
+++ app/week10/page.js
@@ -1,82 +1,90 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { useUserAuth } from "./_utils/auth-context.js";
-import NewItem from './shopping-list/new-item.js';
-import ItemList from './shopping-list/item-list.js';
-import MealIdeas from './shopping-list/meal-ideas.js';
-import { getItems, addItem, deleteItem } from './_services/shopping-list-service.js';
+import { useEffect, useState } from "react";
+import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
+import { getItems, addItem } from "./_services/shopping-list-service"; // import deleteItem as well if needed
 
-function Page() {
+function LandingPage() {
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
     const [items, setItems] = useState([]);
-    const [selectedItemName, setSelectedItemName] = useState('');
-    const { user } = useUserAuth();
+
+    const handleLogin = async () => {
+        await gitHubSignIn();
+    };
+
+    const handleLogout = async () => {
+        await firebaseSignOut();
+    };
 
     const loadItems = async () => {
         if (user) {
-            try {
-                const fetchedItems = await getItems(user.uid);
-                setItems(fetchedItems);
-            } catch (error) {
-                console.error("Error loading items:", error);
-            }
+            const fetchedItems = await getItems(user.uid);
+            setItems(fetchedItems);
         }
     };
 
     useEffect(() => {
         loadItems();
-    }, [user]);
+    }, [user?.uid]);
 
-    const handleAddItem = async (newItem) => {
-        try {
-            const newItemId = await addItem(user.uid, newItem);
-            setItems(prevItems => [...prevItems, { ...newItem, id: newItemId }]);
-        } catch (error) {
-            console.error("Error adding item:", error);
-        }
+    // Handle adding a new item (you can modify as per your UI input method)
+    const handleAddItem = async (newItemName) => {
+        const itemId = await addItem(user.uid, { name: newItemName });
+        setItems([...items, { id: itemId, name: newItemName }]);
     };
 
-    const handleDeleteItem = async (itemId) => {
-        try {
-            await deleteItem(user.uid, itemId);
-            setItems(prevItems => prevItems.filter(item => item.id !== itemId));
-        } catch (error) {
-            console.error("Error deleting item:", error);
-        }
-    };
-
-    const handleItemSelect = (itemName) => {
-        const cleanedName = itemName.split(',')[0].trim().replace(/[^a-zA-Z ]/g, "");
-        setSelectedItemName(cleanedName);
-    };
+    // Optional: Handle deleting an item
+    // const handleDeleteItem = async (itemId) => {
+    //     await deleteItem(user.uid, itemId);
+    //     setItems(items.filter(item => item.id !== itemId));
+    // };
 
     if (!user) {
         return (
             <main className="bg-gray-100 min-h-screen flex items-center justify-center">
                 <div className="bg-white p-8 rounded-lg shadow-md max-w-md mx-auto text-center">
-                    <p className="text-gray-800 text-lg">
-                        Please log in to view the shopping list.
+                    <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+                        Shopping List App
+                    </h1>
+                    <button 
+                        onClick={handleLogin} 
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                    >
+                        Login with GitHub
+                    </button>
+                </div>
+            </main>
+        );
+    } else {
+        return (
+            <main className="bg-gray-100 min-h-screen flex items-center justify-center">
+                <div className="bg-white p-8 rounded-lg shadow-md max-w-md mx-auto text-center">
+                    <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+                        Welcome back!
+                    </h1>
+                    <p className="text-gray-800 text-lg mb-4">
+                        Welcome, {user.displayName} ({user.email})
                     </p>
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.id}>{item.name}</li> // Add delete button if needed
+                        ))}
+                    </ul>
+                    <button 
+                        onClick={handleLogout} 
+                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 mb-4"
+                    >
+                        Logout
+                    </button>
+                    <div className="mt-4">
+                        <Link href="week10/shopping-list" className="text-blue-500 hover:underline">
+                            Go to Shopping List
+                        </Link>
+                    </div>
                 </div>
             </main>
         );
     }
-
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <h1 className="text-3xl font-bold text-gray-800 mb-4">Shopping List</h1>
-            <div style={{ display: 'flex', justifyContent: 'center', width: '100%', gap: '20px' }}>
-                <div>
-                    <h2 className="text-2xl font-bold text-gray-800 mb-4">Add New Item</h2>
-                    <NewItem onAddItem={handleAddItem} />
-                    <ItemList items={items} onItemSelect={handleItemSelect} onDeleteItem={handleDeleteItem} />
-                </div>
-                <div>
-                    <h2 className="text-2xl font-bold text-gray-800 mb-4">Meal Ideas</h2>
-                    <MealIdeas ingredient={selectedItemName} />
-                </div>
-            </div>
-        </div>
-    );
 }
 
-export default Page;
+export default LandingPage;
